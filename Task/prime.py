70e577672397626a60dfa2bff7e0ef3b189240aa
SubhasreeD <117972040+SubhasreeD@users.noreply.github.com>
2023-07-26 07:02:14 -0700

    Update prime.py

--- Task/prime.py
+++ Task/prime.py
@@ -1,11 +1,12 @@
 x=int(input())
 y= int(input())
-for i in range(x,y):
-	count = 2
-	while(i%count!=0):
-		count+=1
-	if(count == i):
-		print("prime")
-	else:
-		print("not a prime")
+if(x>1):
+	for i in range(x,y+1):
+		count = 2
+		while(i%count!=0):
+			count+=1
+		if(count == i):
+			print(i,"is a prime number")
+		else:
+			print(i,"is not prime a number")
 
