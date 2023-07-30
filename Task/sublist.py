6bf0ed090cef459bcce13417f1ecd8acb03ba4d4
SubhasreeD <117972040+SubhasreeD@users.noreply.github.com>
2023-07-30 11:31:20 +0530

    Update sublist.py

--- Task/sublist.py
+++ Task/sublist.py
@@ -1,11 +1,18 @@
-l1=[[1,2,3],[2,30,2],[3,4,5]]
+l1=[[2,30,2],[1,2,3],[3,4,5]]
 max=0
 c=0
+a=0
+mini=sum(l1[0])
 for i in l1:
 	isum=sum(i)
 	if(isum>max):
 		max=isum
 		c=i
-print(c)
-print(l1.index(c))
+for j in l1:
+	isum=sum(j)
+	if(mini>isum):
+		mini=isum
+		a=j	
+print("low value sublist",l1.index(a))
+print("low value sublist",l1.index(c))
 	
